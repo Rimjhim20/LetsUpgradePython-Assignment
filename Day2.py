a8974475de45974d93e2907f0483acd0bfabf0b5
Rimjhim <kumaririmjhim720@gmail.com>
2022-02-21 12:31:36 +0530

    commit

--- Day2.py
+++ Day2.py
@@ -60,3 +60,40 @@
 # 07. Write a program to accept a number from 1 to 12 and display name of the month and days in that month like 1 for January and number of days 31 and so on
 
 
+x = int(input("Enter a number: "))  
+
+if (x==1):
+    print ("Jan")
+if (x==2):
+    print("Feb")
+if (x==3):
+    print("March")
+if (x==4):
+    print("April")
+if (x==5):
+    print("May")
+if (x==6):
+    print("June")
+if (x==7):
+    print("July")
+if (x==8):
+    print("August")
+if(x==9):
+    print("September")
+if(x==10):
+    print("October")
+if(x==11):
+    print("November")
+if(x==12):
+    print("December")
+if(x<1 or x>12):
+    print("Invalid input")
+
+if x == 2:
+	print("No. of days: 28/29 days")
+elif x in (4,6,9,11):
+	print("No. of days: 30 days")
+elif x in  (1, 3, 5, 7, 8, 10, 12):
+	print("No. of days: 31 day")
+else:
+	print("Wrong month name") 
